de8f54a3e722e854080d0a626466a4f6e4ae507d
MelP18 <priscagnansounou99@gmail.com>
2026-08-29 21:19:50 +0100

    Drop unused VueAxios plugin registration

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-
 import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
 
 import { toast } from 'vue3-toastify';
@@ -20,7 +17,6 @@ const pinia = createPinia()
 app.use(router)
 
 app.use(pinia)
-app.use(VueAxios, axios)
 
 app.use(Vue3Toastify, {
     autoClose: 3000,
